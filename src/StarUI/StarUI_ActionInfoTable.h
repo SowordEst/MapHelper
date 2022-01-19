2145b183991e10f7e7ed127328efa5602c44403a
Alan-Est <59730414+Alan-Est@users.noreply.github.com>
2022-01-19 11:27:48 +0800

    测试

--- src/StarUI/StarUI_ActionInfoTable.h
+++ src/StarUI/StarUI_ActionInfoTable.h
@@ -1,6 +1,79 @@
-	{"StarCodeBlockCreate", 
-		{
-			{ Action::Type::action , "WESTRING_ACTIONS" }
-		}, 
+	{"StarCodeBlockCreate",
+{
+		{ Action::Type::action , "WESTRING_ACTIONS" }
+},
 	},
-	//该文件配置为 不参与生成
+	{ "StarExecuteTriggerExReturn",
+{
+	{ Action::Type::action , "WESTRING_ReturnPARAMETERS" }
+},
+	},
+	{ "StarExecuteUnitEventII",
+{
+	{ Action::Type::action , "WESTRING_PARAMETERS" }
+},
+	},
+	{ "StarExecuteUnitEvent",
+{
+	{ Action::Type::action , "WESTRING_PARAMETERS" }
+},
+	},
+	{ "StarExecuteEventII",
+{
+	{ Action::Type::action , "WESTRING_PARAMETERS" }
+},
+	},
+	{ "StarExecuteTriggerEx",
+{
+	{ Action::Type::action , "WESTRING_PARAMETERS" }
+},
+	},
+	{ "EG_ForGroupSW",
+{
+	{ Action::Type::action , "WESTRING_ACTIONS" }
+},
+	},
+	{ "LG_ForGroupSW",
+{
+	{ Action::Type::action , "WESTRING_ACTIONS" }
+},
+	},
+	{ "StarCodeBlockCreate",
+{
+	{ Action::Type::action , "WESTRING_ACTIONS" }
+},
+
+	},
+	{ "EC_ShootReady_EffectEX",
+{
+	{ Action::Type::action , "WESTRING_PARAMETERS" },
+	{ Action::Type::action , "WESTRING_ACTIONS6" }
+},
+
+	},
+	{ "StarCodeBlockExecute",
+{
+	{ Action::Type::action , "WESTRING_PARAMETERS" }
+},
+
+	},
+	{ "StarExecuteEvent",
+{
+	{ Action::Type::action , "WESTRING_PARAMETERS" }
+},
+
+	},
+	{ "EC_ShootReadyEx",
+{
+	{ Action::Type::action , "WESTRING_PARAMETERS" },
+	{ Action::Type::action , "WESTRING_ACTIONS6" }
+},
+
+	},
+	{ "StarES_EX",
+{
+	{ Action::Type::action , "WESTRING_PARAMETERS" },
+	{ Action::Type::action , "WESTRING_ACTIONS4" }
+},
+	},
+//该文件配置为 不参与生成
